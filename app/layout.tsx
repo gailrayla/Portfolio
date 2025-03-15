e8919ae4492a61432cc795920a9e48a7c85c0f42
Gail Rayla Emanuelle Parayno <paraynogail@gmail.com>
2025-03-15 15:22:18 +0800

    Improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,40 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Gail Parayno | Personal Portfolio",
+  title: "Gail Parayno | Software Engineer Portfolio",
   description:
-    "Gail Parayno is a fresh graduate software engineer with 3 years of experience in software development.",
+    "Gail Parayno is a software engineer specializing in frontend and full-stack development. Explore projects, skills, and experience in web development.",
+  keywords: [
+    "Gail Parayno",
+    "Software Engineer Portfolio",
+    "Frontend Developer",
+    "Backend Developer",
+    "Full-Stack Developer",
+    "Web Development",
+  ],
+  openGraph: {
+    title: "Gail Parayno | Software Engineer Portfolio",
+    description:
+      "Gail Parayno is a software engineer specializing in frontend and full-stack development.",
+    url: "https://www.gailparayno.com/",
+    siteName: "Gail Parayno Portfolio",
+    images: [
+      {
+        url: "public/website-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Gail Parayno Portfolio",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Gail Parayno | Software Engineer Portfolio",
+    description:
+      "Gail Parayno is a software engineer specializing in frontend and full-stack development.",
+    images: ["public/website-image.png"],
+  },
 };
 
 export default function RootLayout({
@@ -25,6 +56,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className="!scroll-smooth">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Person",
+              name: "Gail Parayno",
+              jobTitle: "Software Engineer",
+              url: "https://www.gailparayno.com/",
+              sameAs: [
+                "https://www.linkedin.com/in/gail-parayno-280644247/",
+                "https://github.com/gailrayla",
+              ],
+            }),
+          }}
+        />
+      </head>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
