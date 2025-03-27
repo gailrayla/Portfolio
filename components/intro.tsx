40d6a69d4ed6998a32298b93872a9091b55ae66c
Gail Rayla Emanuelle Parayno <paraynogail@gmail.com>
2025-03-27 14:02:40 +0800

    CV Update

--- components/intro.tsx
+++ components/intro.tsx
@@ -90,7 +90,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10"
-          href="/Gail_Rayla_Emanuelle_Parayno_CV.pdf"
+          href="/Gail_Parayno_CV_Resume.pdf"
           download
         >
           Download CV{" "}
